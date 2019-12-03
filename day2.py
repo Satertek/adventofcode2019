ed4e543d1e7dbb4b33c681fda6a13155df86bc33
Irelan <1387910855@mil>
2019-12-03 13:01:30 -0600

    misc cleanup

--- day2.py
+++ day2.py
@@ -3,7 +3,7 @@ import sys
 
 PART_ONE = False
 
-input = np.loadtxt("day2_input.txt", dtype=int, delimiter=',')
+day2_in = np.loadtxt("day2_input.txt", dtype=int, delimiter=',')
 
 if PART_ONE:
     noun = 12
@@ -14,7 +14,7 @@ else:
 
 while noun < 99:
     while verb < 99:   
-        output = input.copy()
+        output = day2_in.copy()
         output[1] = noun
         output[2] = verb
         i = 0
@@ -27,16 +27,11 @@ while noun < 99:
                 verb += 1
                 break
             else:
-                print(f"ERROR: Unknown Intcode: {output[i]} (Noun: {noun}, Verb: {verb})")
-                sys.exit()
+                sys.exit(f"Invalid opcode {output[i]}. Something went wrong!")
             i += 4
         if output[0] == 19690720 or PART_ONE:
-            print(f"Position [0] Result: {output[0]}")
-            print(f"Position [1] Result: {output[1]}")
-            print(f"Position [2] Result: {output[2]}")
+            print(f"Position [0,1,2]: [{output[0]}, {output[1]}, {output[2]}]")
             print(f"Final Output: {100 * output[1] + output[2]}")
-            output[1] = 1e6
-            output[2] = 1e6
             break
     if output[0] == 19690720 or PART_ONE:
         break
